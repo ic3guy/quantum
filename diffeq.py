964fe88abf5b9f2359a47dfa4450a4257ad9901a
William Denman <william.denman@gmail.com>
2012-10-10 16:46:31 +0100

    got it working. key was substituion from dictionary

--- diffeq.py
+++ diffeq.py
@@ -1,43 +1,27 @@
 from __future__ import division
 from sympy import *
 
+t = Symbol('t')
+x1 = Function('x1')(t)
+x2 = Function('x2')(t)
 
-def gen_dif_equations (n, *variables, **derivatives):
+deriv_dict = {x1.diff(t): x2,
+              x2.diff(t): sin(x1)*cos(x1)-10*sin(x1)}
 
-    t = symbols('t')
-    varlist = []
-    derlist = []
-    
-    for var in variables:
-        print var
-        varlist.append(Function(var)(t))
-        #varlist.append(var)
-        
-    for eq in derivatives:
-        #print eq
-        eq = Function(str(eq))(t)
-      #  eq = sympify(derivatives[eq])
-        derlist.append(eq)
-        #print eq
-        #print eq.diff(t)
-        
-        v = eq.diff(t)
+#x1d = x2(t)
+#x2d = sin(x1(t))*cos(x1(t))-10*sin(x1(t))
 
-        print "Varlist: " + str(varlist)
-        
-        print "V: " + str(v)
+odelist = []
 
-    print "Derlist: " + str(derlist)
+#diff(sin(x1)*cos(x1)-10*sin(x1)).subs(deriv_dict)
+#print diff(sin(x1)*cos(x1)-10*sin(x1)).subs(deriv_dict).diff().subs(deriv_dict)
+#print diff(sin(x1)*cos(x1)-10*sin(x1)).subs(deriv_dict).diff().subs(deriv_dict).diff().subs(deriv_dict)
 
-    for eq in derlist:
-        print eq
-        
-    for var in varlist:
-        print "Var: " + repr(var)
-        print sympify(var).diff(t)
-        print v.subs(sympify(var).diff(t),2) 
-            #v = v.subs(cos,sin)
-        print v
-    
-    #for i in range(n):
-        
+def get_derivs (n, seed):
+
+    for n in range(n):
+        print str(n) + " : "  + str(diff(seed).subs(deriv_dict))
+        seed = diff(seed).subs(deriv_dict)
+
+
+get_derivs(3, sin(x1)*cos(x1)-10*sin(x1))

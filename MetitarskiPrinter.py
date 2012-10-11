ea5a6fe64ff6f8bbb03cce0c49793508c58b3255
William Denman <william.denman@gmail.com>
2012-10-11 15:28:47 +0100

    MetiPrinter working, must reload interpreter

---
+++ MetitarskiPrinter.py
@@ -0,0 +1,104 @@
+from sympy.printing.str import StrPrinter
+from sympy.printing.precedence import precedence
+from sympy.core import S
+from sympy.polys.polyerrors import PolynomialError
+
+class MetitarskiPrinter(StrPrinter):
+
+    printmethod = "_metitarski"
+
+   # _default_settings = {}
+
+    #_translation_table = {
+        #'**':'^',
+   #     'x':'X',
+   #     'y':'Y'
+   # }
+
+    def _print_Pow(self, expr, rational=False):
+        PREC = precedence(expr)
+
+        if expr.exp is S.Half and not rational:
+            return "sqrt(%s)" % self._print(expr.base)
+
+        if expr.is_commutative:
+            if -expr.exp is S.Half and not rational:
+                # Note: Don't test "expr.exp == -S.Half" here, because that will
+                # match -0.5, which we don't want.
+                return "1/sqrt(%s)" % self._print(expr.base)
+            if expr.exp == -1:
+                return '1/%s' % self.parenthesize(expr.base, PREC)
+
+        e = self.parenthesize(expr.exp, PREC)
+        if self.printmethod == '_metitarski' and expr.exp.is_Rational and expr.exp.q != 1:
+            # the parenthesized exp should be '(Rational(a, b))' so strip parens,
+            # but just check to be sure.
+            if e.startswith('(Rational'):
+                return '%s^%s' % (self.parenthesize(expr.base, PREC), e[1:-1])
+        return '%s^%s' % (self.parenthesize(expr.base, PREC), e)
+
+    def _print_Poly(self, expr):
+        terms, gens = [], [ self._print(s) for s in expr.gens ]
+
+        for monom, coeff in expr.terms():
+            s_monom = []
+
+            for i, exp in enumerate(monom):
+                if exp > 0:
+                    if exp == 1:
+                        s_monom.append(gens[i])
+                    else:
+                        s_monom.append(gens[i] + "^%d" % exp)
+
+            s_monom = "*".join(s_monom)
+
+            if coeff.is_Add:
+                if s_monom:
+                    s_coeff = "(" + self._print(coeff) + ")"
+                else:
+                    s_coeff = self._print(coeff)
+            else:
+                if s_monom:
+                    if coeff is S.One:
+                        terms.extend(['+', s_monom])
+                        continue
+
+                    if coeff is S.NegativeOne:
+                        terms.extend(['-', s_monom])
+                        continue
+
+                s_coeff = self._print(coeff)
+
+            if not s_monom:
+                s_term = s_coeff
+            else:
+                s_term = s_coeff + "*" + s_monom
+
+            if s_term.startswith('-'):
+                terms.extend(['-', s_term[1:]])
+            else:
+                terms.extend(['+', s_term])
+
+        if terms[0] in ['-', '+']:
+            modifier = terms.pop(0)
+
+            if modifier == '-':
+                terms[0] = '-' + terms[0]
+
+        format = expr.__class__.__name__ + "(%s, %s"
+
+        try:
+            format += ", modulus=%s" % expr.get_modulus()
+        except PolynomialError:
+            format += ", domain='%s'" % expr.get_domain()
+
+        format += ")"
+
+        return format % (' '.join(terms), ', '.join(gens))
+
+
+    def _print_MatPow(self, expr):
+        PREC = precedence(expr)
+
+        return '%s^%s'%(self.parenthesize(expr.base, PREC),
+                         self.parenthesize(expr.exp, PREC))
--- diffeq.py
+++ diffeq.py
@@ -1,12 +1,21 @@
 from __future__ import division
 from sympy import *
 from sympy.plotting.plot import Plot
+from MetitarskiPrinter import MetitarskiPrinter
+
+
+def metitarski(expr, **settings):
+    """Transform an expression to a string with Mathematica syntax. """
+    p = MetitarskiPrinter(settings)
+    s = p.doprint(expr)
+
+    return s
 
 #from matplotlib import *
 
 t = Symbol('t')
-x = Symbol('x')
-y = Symbol('y')
+X1 = Symbol('X1')
+X2 = Symbol('X2')
 x1 = Function('x1')(t)
 x2 = Function('x2')(t)
 
@@ -15,7 +24,7 @@ x2 = Function('x2')(t)
 deriv_dict = {x1.diff(t): x2,
               x2.diff(t): sin(x1)*cos(x1)-10*sin(x1)}
 
-plot_dict = {x1(t):x,x2(t):y}
+plot_dict = {x1(t):X1,x2(t):X2}
 #x1d = x2(t)
 #x2d = sin(x1(t))*cos(x1(t))-10*sin(x1(t))
 
@@ -30,24 +39,26 @@ def get_derivs (n, seed):
     for n in range(n):
 
         dn = diff(seed).subs(deriv_dict)
-        
+
         #print str(n) + " : "  + str(dn)
         #plot(diff(seed).subs(deriv_dict))
         plotlist.append(dn.subs(plot_dict))
         seed = diff(seed).subs(deriv_dict)
-        
 
-get_derivs(6, sin(x1)*cos(x1)-10*sin(x1))
-
-x = Symbol('x')
+get_derivs(3, sin(x1)*cos(x1)-10*sin(x1))
+#print plotlist
 
 p = Plot()
 
+# from sys import *
+#getrecursionlimit()
+#setrecursionlimit()
+
 for plot in plotlist:
-    p.extend(plot_implicit(Eq(plot,0), (x,-pi,pi),(y,-pi,pi), adaptive=False, show=False))
+    p.extend(plot_implicit(Eq(plot,0), (X1,-2,2),(X2,-2,2), adaptive=False, show=False, points=400))
 
 p.show()
 
 
-
-
+for plot in plotlist:
+    print metitarski(plot)

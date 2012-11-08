d5ba705168bf6cc946ed6438980359f59afb61ad
William Denman <william.denman@gmail.com>
2012-11-08 17:37:30 +0000

    examples editied

--- metitarski.py
+++ metitarski.py
@@ -7,13 +7,14 @@ import uuid
 metit_options = ('metit', 
                  '--autoInclude', 
                  '--time','1',
+                 '-q',
                  '-')
 
 process = None
 
 def send_to_metit(fof,output=False,tofile=False,metit_options=metit_options):
     if output:
-        print fof
+        #print fof
         process = subprocess.Popen(metit_options, stdin=subprocess.PIPE)
     else:
         process = subprocess.Popen(metit_options, shell=False, stdout=open('/dev/null','w'), stdin=subprocess.PIPE)
@@ -120,9 +121,10 @@ def checkTransition2(state, pred, x):
     Q1,Q2,Q3 = [],[],[]
 
     options = ('metit', 
-           '--autoInclude', 
-           '--time','30',
-           '-')
+               '--autoInclude', 
+               '--time','1',
+               '-q',
+               '-')
     
     der = pred.derivative
     #pre = predicate.MetitEquation(pred.equation.equation,pred.equation.depvar,pred.equation.subs_dict,pred.equation.vars_dict)
@@ -133,25 +135,25 @@ def checkTransition2(state, pred, x):
     gteq = make_fof_rel_2(state,der,'>', '=')
 
     if pred.operator == '>' or pred.operator == '=':
-        if not send_to_metit(gteq, output=True):
+        if not send_to_metit(gteq, output=True,metit_options=options):
             Q1.append(state)
-            print 'In Q1'
+            #print 'In Q1'
         else: 
             send_to_file(gteq,'unproved', 'Q1--S_%s--P_%s--O_%s--I_gteq' % (state.number, x, pred_2_text(pred.operator)))
     
     if pred.operator == '<' or pred.operator == '=':
-        if not send_to_metit(lteq, output=True):
+        if not send_to_metit(lteq, output=True,metit_options=options):
             Q3.append(state)
-            print 'In Q3'
+            #print 'In Q3'
         else:
-            send_to_file(lteq,'unproved', 'Q2--S_%s--P_%s--O_%s--I_lteq' % (state.number, x, pred_2_text(pred.operator)))
+            send_to_file(lteq,'unproved', 'Q3--S_%s--P_%s--O_%s--I_lteq' % (state.number, x, pred_2_text(pred.operator)))
     
     if pred.operator == '=':
-        if not send_to_metit(gt_or_lt,output=True):
+        if not send_to_metit(gt_or_lt,output=True,metit_options=options):
             Q2.append(state)
-            print 'In Q2'
+            #print 'In Q2'
         else:
-            send_to_file(gt_or_lt,'unproved', 'Q3--S_%s--P_%s--O_%s--I_neq' % (state.number, x, pred_2_text(pred.operator)))
+            send_to_file(gt_or_lt,'unproved', 'Q2--S_%s--P_%s--O_%s--I_neq' % (state.number, x, pred_2_text(pred.operator)))
 
     return (Q1,Q2,Q3)
 

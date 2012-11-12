f9c31cc4f5b9ecb6154ea788dd981db6eadb90bd
William Denman <william.denman@gmail.com>
2012-11-12 15:16:59 +0000

    fixing nusmv output function

--- nusmv.py
+++ nusmv.py
@@ -1,37 +1,42 @@
 import predicate
 
-def transition_relation(cur_state,next_states, system):
-    
-    #filter out next states that have been shown to have no next state (deleted)
-    next_states = [n for n in next_states if system[n].is_feasible] 
-    
-    if len(next_states) == 1:
-        next_states_str = str(next_states[0])
+def transition_relation(cur_state,next_states, system):  
+    if next_states:
+        if len(next_states) == 1:
+            next_states_str = str(next_states[0])
+        else:
+            next_states_str = ','.join(map(str,next_states))
+            next_states_str = '{' + next_states_str + '}'
     else:
-        next_states_str = ','.join(map(str,next_states))
-        next_states_str = '{' + next_states_str + '}'
+        system[cur_state].is_feasible = False
     
-    return '\t\tstate = %s: %s;\n' % (cur_state, next_states_str)
+        return '\t\tstate = %s: %s;\n' % (cur_state, next_states_str)
     
 def construct_nusmv_input(system, init_state):
-    transition = 'MODULE main\nVAR\n\t'
-
+    case_block = construct_transition_case_block(system)
     states = ','.join([s.get_state_number() for s in system if s.is_feasible])
+
+    nusmv_output = 'MODULE main\nVAR\n\t'
+    nusmv_output += 'state : {%s};\nASSIGN\n\t' % states
+    nusmv_output += 'init(state) := %s;\n' % str(init_state)
+    nusmv_output += case_block
+
+    print nusmv_output
+
+def construct_transition_case_block(system):
+    case_block = '\tnext(state) := case\n'
     
-    transition = transition + 'state : {%s};\nASSIGN\n\t' % states
-
-    transition = transition + 'init(state) := %s;\n' % str(init_state)
-    transition = transition + '\tnext(state) := case\n'
-
     for tr in system:
-        if tr.is_feasible:
-            transition = transition + transition_relation(tr.get_state_number(),tr.next_states,system)
+        #filter out next states that have been shown to have no next state (deleted)
+        next_states = [n for n in tr.next_states if system[n].is_feasible]
+        
+        if tr.is_feasible and next_states:
+            case_block += transition_relation(tr.get_state_number(),next_states,system)
     
-    transition = transition + ('\tesac;\n')
+    case_block += '\tesac;\n'
+    
+    return case_block
 
-    print transition
-    
-    
 if __name__ == '__main__':
 
     e1 = predicate.MetitEquation('x(t)','t',{},{})

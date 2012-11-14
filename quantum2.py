bcf0bf3ee2a8bc4b1302d3b8a5841456a1b5aaa0
William Denman <william.denman@gmail.com>
2012-11-14 13:03:32 +0000

    now have examples in external files

--- quantum2.py
+++ quantum2.py
@@ -13,33 +13,15 @@ def secondsToStr(t):
         reduce(lambda ll,b : divmod(ll[0],b) + ll[1:],
             [(t*1000,),1000,60,60])
 
-t = Symbol('t')
-X1 = Symbol('X1')
-X2 = Symbol('X2')
-x1 = Function('x1')(t)
-x2 = Function('x2')(t)
 
+execfile('simplePendulum.py')
 start_time = time.time()    
-    
-deriv_dict = {x1.diff(t): x2,
-              x2.diff(t): -9.8*sin(x1)}
-
-vars_dict = {x1(t) : X1, x2(t) : X2}
-    
-equations = [predicate.MetitEquation(x1,'t',deriv_dict,vars_dict),
-             predicate.MetitEquation(x2,'t',deriv_dict,vars_dict)]
-             #predicate.MetitEquation(-9.8*sin(x1),'t',deriv_dict,vars_dict),
-             #predicate.MetitEquation(x1-1,'t',deriv_dict,vars_dict),
-             #predicate.MetitEquation(x2-15,'t',deriv_dict,vars_dict)]
-             #predicate.MetitEquation(0.3345*x2**2+1.4615*sin(x1)**2+1.7959*cos(x1)**2-6.689*cos(x1)+4.6931-15, 't',deriv_dict, vars_dict)]
-
-e5 = predicate.MetitEquation(deriv_dict[x2.diff(t)],'t',deriv_dict,vars_dict)
-equations.extend(predicate.get_derivs(1,e5))
 
 feasible = 0
 infeasible = 0
 oplist = ['>','=','<']
 inftest = []
+system = []
     
 for equation in equations:
     predlist = [predicate.MetitPredicate(equation,op) for op in oplist]

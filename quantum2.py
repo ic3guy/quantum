2f3f8447d0e44a0558bfa2e53629543feda74e32
William Denman <william.denman@gmail.com>
2012-11-14 14:09:03 +0000

    constructing abstract initial state

--- quantum2.py
+++ quantum2.py
@@ -12,8 +12,7 @@ def secondsToStr(t):
     return "%d:%02d:%02d.%03d" % \
         reduce(lambda ll,b : divmod(ll[0],b) + ll[1:],
             [(t*1000,),1000,60,60])
-
-
+           
 execfile('simplePendulum.py')
 start_time = time.time()    
 

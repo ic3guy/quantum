bd85088f67f2f19dc4afc3130768d9d18be9cc6e
William Denman <william.denman@gmail.com>
2012-12-10 01:06:13 +0000

    added invariants, much faster

--- bounceBallsin.py
+++ bounceBallsin.py
@@ -20,6 +20,19 @@ vars_dict = {px : PX, py : PY, vx : VX, vy: VY}
 guard_equation = predicate.MetitEquation(sin(px)-py,'t',[],vars_dict)
 guard = predicate.MetitPredicate(guard_equation,'=')
 
+inv = predicate.MetitEquation(px,'t',[],vars_dict)
+inv_pred_pxlz = predicate.MetitPredicate(inv, '<')
+
+inv = predicate.MetitEquation(py,'t',[],vars_dict)
+inv_pred_pylz = predicate.MetitPredicate(inv, '<')
+
+inv = predicate.MetitEquation(vx,'t',[],vars_dict)
+inv_pred_vxlz = predicate.MetitPredicate(inv, '<')
+
+inv = predicate.MetitEquation(vy,'t',[],vars_dict)
+inv_pred_vylz = predicate.MetitPredicate(inv, '<')
+
+
 deriv_dict = {('falling',) : {'flow' : {px.diff(t): vx,
                                         py.diff(t): vy,
                                         vx.diff(t): 0,
@@ -28,7 +41,7 @@ deriv_dict = {('falling',) : {'flow' : {px.diff(t): vx,
                                       'next_state' : ('falling',),
                                       'updates' : {vx : ((1-0.8*cos(px)**2)*vx + 1.8*cos(px)*vy)/(1+cos(px)**2), 
                                                    vy : (1.8*cos(px)*vx + (-0.8+cos(px)**2)*vy)/(1+cos(px)**2)}}],
-                              'inv' : []}}
+                              'inv' : (inv_pred_vylz, inv_pred_vxlz, inv_pred_pylz, inv_pred_pxlz)}}
 
 
 

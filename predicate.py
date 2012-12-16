761dc986d1982871d8cc02681c1180c22a9b819f
William Denman <william.denman@gmail.com>
2012-12-16 16:09:00 +0000

    fixed derivative taking

--- predicate.py
+++ predicate.py
@@ -13,7 +13,7 @@ def get_derivs(n, seed):
     derivatives = []
 
     for n in range(n):
-        dn = MetitEquation(seed.equation.diff(seed.depvar).subs(seed.subs_dict[('cont',)]['flow']).subs(seed.vars_dict),seed.depvar,seed.subs_dict,seed.vars_dict)
+        dn = MetitEquation(seed.equation.diff(seed.depvar).subs(seed.subs_dict[('cont',)]['flow']),'t',seed.subs_dict,seed.vars_dict)
         derivatives.append(dn)
         seed = dn
 
--- simplePendulum3.py
+++ simplePendulum3.py
@@ -17,8 +17,8 @@ deriv_dict = {('cont',): {'flow': {x1.diff(t): x2,
 vars_dict = {x1 : X1, x2 : X2}
     
 equations = [predicate.MetitEquation(x1,'t',deriv_dict,vars_dict),
-             predicate.MetitEquation(x2,'t',deriv_dict,vars_dict),
-             predicate.MetitEquation(1.90843655*sin(x1)**2 + 1.90843655*cos(x1)**2 - 3.916868466*cos(x1) + 0.19984*x2**2 + 0.0084319171,'t',deriv_dict,vars_dict,is_lyapunov=True)]
+             predicate.MetitEquation(x2,'t',deriv_dict,vars_dict)]
+#predicate.MetitEquation(1.90843655*sin(x1)**2 + 1.90843655*cos(x1)**2 - 3.916868466*cos(x1) + 0.19984*x2**2 + 0.0084319171,'t',deriv_dict,vars_dict,is_lyapunov=True)]
 
-#e5 = predicate.MetitEquation(deriv_dict[('cont',)]['flow'][x2.diff(t)],'t',deriv_dict,vars_dict)
-#equations.extend(predicate.get_derivs(1,e5))
+e5 = predicate.MetitEquation(deriv_dict[('cont',)]['flow'][x2.diff(t)],'t',deriv_dict,vars_dict)
+equations.extend(predicate.get_derivs(2,e5))

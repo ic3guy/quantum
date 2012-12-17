cfb8e8d9bcd648f85561e394247e44fe28f95741
William Denman <william.denman@gmail.com>
2012-12-17 18:50:55 +0000

    just added files

---
+++ bounceBallsin2b.py
@@ -0,0 +1,56 @@
+from sympy import *
+from itertools import product
+#product is a name inside sympy, so we can't redefine it here. Otherwise import won't work
+
+t = Symbol('t')
+PX = Symbol('PX') #x1 = vx
+PY = Symbol('PY') #x2 = vy
+VX = Symbol('VX')
+VY = Symbol('VY')
+
+px = Function('px')(t)
+py = Function('py')(t)
+vx = Function('vx')(t)
+vy = Function('vy')(t)
+
+q = [('falling',)] #can just get dictionary keys...
+
+bad = True
+
+vars_dict = {px : PX, py : PY, vx : VX, vy: VY}
+
+guard_equation = predicate.MetitEquation(sin(px)-py,'t',[],vars_dict)
+guard = predicate.MetitPredicate(guard_equation,'=')
+g_inv = predicate.MetitPredicate(guard_equation,'<')
+
+inv = predicate.MetitEquation(px,'t',[],vars_dict)
+inv_pred_pxlz = predicate.MetitPredicate(inv, '<')
+
+inv = predicate.MetitEquation(py,'t',[],vars_dict)
+inv_pred_pylz = predicate.MetitPredicate(inv, '<')
+
+inv = predicate.MetitEquation(vx,'t',[],vars_dict)
+inv_pred_vxlz = predicate.MetitPredicate(inv, '<')
+
+inv = predicate.MetitEquation(vy,'t',[],vars_dict)
+inv_pred_vylz = predicate.MetitPredicate(inv, '<')
+
+
+deriv_dict = {('falling',) : {'flow' : {px.diff(t): vx,
+                                        py.diff(t): vy,
+                                        vx.diff(t): 0,
+                                        vy.diff(t): (-9.8 + 0.01*vy**2)*(1-exp(-3*t))},
+                              't' : [{'guard':(guard,), 
+                                      'next_state' : ('falling',),
+                                      'updates' : {vx : ((1-0.8*cos(px)**2)*vx + 1.8*cos(px)*vy)/(1+cos(px)**2), 
+                                                   vy : (1.8*cos(px)*vx + (-0.8+cos(px)**2)*vy)/(1+cos(px)**2)}}],
+                              'inv' : (g_inv,)}}
+
+
+
+equations = [predicate.MetitEquation(px,'t',deriv_dict,vars_dict),
+             predicate.MetitEquation(py,'t',deriv_dict,vars_dict),
+             predicate.MetitEquation(vx,'t',deriv_dict,vars_dict),
+             predicate.MetitEquation(vy,'t',deriv_dict,vars_dict),
+             guard_equation]
+
---
+++ top_level.py
@@ -0,0 +1,10 @@
+#filenames = ['simplePendulum4.py','simplePendulum3.py','simplePendulum2.py','simplePendulum4b.py','simplePendulum3b.py','simplePendulum2b.py']
+#filenames = ['simplePendulum.py','simplePendulumbad.py','simplePendulum1.py','simplePendulum1b.py']
+#filenames = ['simplePendulum4.py','simplePendulum4b.py']
+#filenames = ['bounceBallsin2a.py','bounceBallsin2b.py','bounceBallsin.py','bounceBallsinb.py']
+filenames = ['bounceBallsin.py']
+
+for file_name in filenames:
+    exp_name = file_name
+    execfile('/Users/will/Research/quantum/quantum4-log.py')
+

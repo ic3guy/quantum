f97aeacd4118f0a704a924fb454035b073580926
William Denman <william.denman@gmail.com>
2013-01-29 23:22:26 +0000

    some more edits

--- top_level.py
+++ top_level.py
@@ -1,8 +1,8 @@
 #filenames = ['simplePendulum4.py','simplePendulum3.py','simplePendulum2.py','simplePendulum4b.py','simplePendulum3b.py','simplePendulum2b.py']
 #filenames = ['simplePendulum','simplePendulumbad','simplePendulum1','simplePendulum1b']
 #filenames = ['simplePendulum4.py']
-filenames = ['bounceBallsin2a','bounceBallsin2b','bounceBallsin','bounceBallsinb']
-#filenames = ['bounceBallsin.py']
+#filenames = ['bounceBallsin2a','bounceBallsin2b','bounceBallsin','bounceBallsinb']
+filenames = ['bounceBallsin']
 
 for file_name in filenames:
     exp_name = file_name + '.py'

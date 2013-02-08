b44f0d63c9ac62a65ecdeed0348227f4f08ee328
William Denman <william.denman@gmail.com>
2013-02-08 16:50:03 +0000

    changing base class MetitEquation

--- predicate.py
+++ predicate.py
@@ -5,8 +5,13 @@ from sympy import diff
 import metitarski
 from itertools import product
 import MetitarskiPrinter
+import re
+
+#to find all functions.
+from sympy.core.function import AppliedUndef
 
 assert diff
+
 
 def get_derivs(n, seed):
 
@@ -19,28 +24,34 @@ def get_derivs(n, seed):
 
     return derivatives
     
-def metitarski_pp(expr, **settings):
-    """Transform an expression to a string with Mathematica syntax. """
-    p = MetitarskiPrinter.MetitarskiPrinter(settings)
-    s = p.doprint(expr)
-
-    return s
-
 class MetitEquation:
-    def __init__(self,equation,depvar,subs_dict,vars_dict,is_lyapunov=False):
-        self.equation = sympify(equation)
+    def __init__(self, equation, depvar=Symbol('t'), is_lyapunov=False):
+        self.equation = equation
         #self.derivative = sympify(equation).diff(depvar).subs(subs_dict)
         self.depvar = depvar
-        self.subs_dict = subs_dict
-        self.vars_dict = vars_dict
+        #self.subs_dict = subs_dict
         self.is_lyapunov = is_lyapunov
         
-    def __str__(self):
-        return metitarski_pp(self.equation.subs(self.vars_dict))
+    def __str__(self, subsdict={'e':'*10^', '**':'^'}):
+
+        #this gives us the functions
+        var_list = [[var, sympify(str(var).replace("("+str(self.depvar)+")","").upper())] for var in self.equation.atoms(AppliedUndef)]
+        print var_list
+        # any other variables
+        var_list.extend([[var, sympify(str(var).upper())] for var in self.equation.free_symbols])
+        print var_list
+
+        #print into a format by metitarski)
+        rep = dict((re.escape(k), v) for k, v in subsdict.iteritems())
+        #print rep
+        pattern = re.compile("|".join(rep.keys()))
+        equation_out = pattern.sub(lambda m: subsdict[m.group(0)], str(self.equation.subs(var_list)))
+        
+        return equation_out
 
     def print_derivative(self):
         if self.is_lyapunov:
-            return metitarski_pp(self.derivative.subs(self.vars_dict)) + '-10^-2'
+            return str(self) + '-10^-2'
         else:
             return metitarski_pp(self.derivative.subs(self.vars_dict))
         
@@ -112,73 +123,18 @@ class State:
             return metitarski_pp(pred.equation.equation.diff(pred.equation.depvar).subs(self.deriv_dict[self.discrete_part]['flow']).subs(pred.equation.vars_dict))
 
        
-
-    
 if __name__ == '__main__':
-
-    e1 = MetitEquation('x(t)','t',{},{})
-    e2 = MetitEquation('x(t)','t',{},{})
-    
-    p1 = MetitPredicate(e1,'<')
-    p2 = MetitPredicate(e2,'>')
-    p3 = MetitPredicate(e2, '>')
-    s1 = State('X,Y',p1,p2,p3)
-    p = metitarski.make_fof_inf(s1)
-    #metitarski.send_to_metit(p)
-    
     t = Symbol('t')
-    X1 = Symbol('X1')
-    X2 = Symbol('X2')
-
     x1 = Function('x1')(t)
     x2 = Function('x2')(t)
+    a = Symbol('a')
     
+    x = MetitEquation(-9.8*sin(x1(t)))
+    y = MetitEquation(x2(t)+a)
+    z = MetitEquation(1.90843655*sin(x1(t))**2 + 1.90843655*cos(x1(t))**2 - 3.916868466*cos(x1(t)) + 0.19984*x2(t)**2 - 0.0084319171)
+
+    flow = {x1.diff(t): x2, x2.diff(t): -9.8*sin(x1)}
     
-    deriv_dict = {x1.diff(t): x2,
-                  x2.diff(t): sin(x1)*cos(x1)-10*sin(x1)}
-
-    vars_dict = {x1(t) : X1, x2(t) : X2}
-    
-    #def get_derivs(n, seed):
-     #   dn = diff(seed).subs(deriv_dict)
-
-    #e1 = MetitEquation('x2(t)','t',deriv_dict)
-    #e2 = MetitEquation('sin(x1(t))*cos(x1(t))-10*sin(x1(t))','t',deriv_dict)
-
-#    gen_equations = 
-
-    #equations_initial = [MetitEquation(x1,'t',deriv_dict,vars_dict), MetitEquation(x2,t,deriv_dict,vars_dict), MetitEquation(sin(x1)*cos(x1)-10*sin(x1),t,deriv_dict,vars_dict)]
-
-    
-    #equations = [MetitEquation(x,'t',deriv_dict,vars_dict) for x in deriv_dict.values()]
-
-    #equations = [MetitEquation(x1,'t',deriv_dict,vars_dict),
-    #             MetitEquation(x2,'t',deriv_dict,vars_dict),
-    #             MetitEquation(sin(x1)*cos(x1)-10*sin(x1),'t',deriv_dict,vars_dict),
-   #              MetitEquation(0.3345*x2**2+1.4615*sin(x1)**2+1.7959*cos(x1)**2-6.689*cos(x1)+4.6931, 't',deriv_dict, vars_dict)]
-
-   # e5 = MetitEquation(deriv_dict[x2.diff(t)],'t',deriv_dict,vars_dict)
-    
-    #equations.extend(e4)
-    #equations.extend(get_derivs(1,e5))
-
-    #feasible = 0
-    #infeasible = 0
-    #oplist = ['>','<']
-    #inftest = []
-    
-   # for equation in equations:
-    #    predlist = [MetitPredicate(equation,op) for op in oplist]
-    #    inftest.append(predlist)
-
-    #system = [State('X1,X2',*element) for element in product(*inftest)]
-
-    #for state in system:
-    #    print metitarski.make_fof_inf(state)
-    #    rc = metitarski.send_to_metit(metitarski.make_fof_inf(state),output=False)
-    #    if rc == 0: infeasible = infeasible+1
-    #    if rc == 1: feasible = feasible+1
-
-    #print "Feasible %s" % feasible
-    #print "Infeasible %s" % infeasible
-
+    print x
+    print y
+    print z

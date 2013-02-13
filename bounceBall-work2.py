8a079727f8657ba80f370dcf8a498da6f680b131
William Denman <william.denman@gmail.com>
2013-02-13 17:14:24 +0000

    added new files

---
+++ bounceBall-work2.py
@@ -0,0 +1,44 @@
+from sympy import *
+from itertools import product
+#product is a name inside sympy, so we can't redefine it here. Otherwise import won't work
+
+t = Symbol('t')
+py = Function('py')(t)
+vy = Function('vy')(t)
+
+q = [('falling',)] #can just get dictionary keys...
+
+bad = False
+
+#guard_equation = predicate.MetitEquation(sin(px)-py,'t',[],vars_dict)
+
+guard_equation = py
+guard = predicate.MetitPredicate(guard_equation,'=')
+g_inv = predicate.MetitPredicate(guard_equation,'<')
+
+#inv = predicate.MetitEquation(px,'t',[],vars_dict)
+#inv_pred_pxlz = predicate.MetitPredicate(px, '<')
+
+#inv = predicate.MetitEquation(py,'t',[],vars_dict)
+inv_pred_pylz = predicate.MetitPredicate(py, '<')
+
+#inv = predicate.MetitEquation(vx,'t',[],vars_dict)
+#inv_pred_vxlz = predicate.MetitPredicate(vx, '<')
+
+#inv = predicate.MetitEquation(vy,'t',[],vars_dict)
+#inv_pred_vylz = predicate.MetitPredicate(vy, '<')
+
+
+system_def = {('falling',) : {'flow' : {py.diff(t): vy,
+                                        vy.diff(t): -9.8},
+                              't' : [{'guard':(guard,), 
+                                      'next_state' : ('falling',),
+                                      'updates' : {vy : -0.2*vy}}],
+                              'inv' : (g_inv,)}}
+
+
+equations = [predicate.MetitEquation(py),
+             predicate.MetitEquation(vy),
+             predicate.MetitEquation(py-1),
+             predicate.MetitEquation(vy+9.8)]
+

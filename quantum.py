3732d3fdf07943512babb7af604f8e396a4802cf
William Denman <william.denman@gmail.com>
2013-05-09 15:49:59 -0700

    removed pass numbered versions of quantum

--- quantum.py
+++ quantum.py
@@ -2,134 +2,97 @@ from sympy import *
 import metitarski
 from itertools import product
 import predicate
+import abstraction
 import datetime
 import os
 import nusmv
-import timing
+#import timing
+import time
+from termcolor import colored, cprint
+import qutilities
+import experiment
 
-t = Symbol('t')
-X1 = Symbol('X1')
-X2 = Symbol('X2')
-x1 = Function('x1')(t)
-x2 = Function('x2')(t)
-    
-timing.log("Start Program")
-    
-deriv_dict = {x1.diff(t): x2,
-              x2.diff(t): -9.8*sin(x1)}
+#filenames = ['twotanks']
+#filenames = ['heater-new-timed']
+#filenames = ['bounceBallsin-new5c']
+#filenames = ['simplePendulum3-new','simplePendulum4-new']
+#filenames = ['simplePendulum-new','simplePendulum2-new',]
+filenames = ['simplePendulum4-new']
+#filenames = ['simplePendulum-new','simplePendulum2-new','simplePendulum3-new','simplePendulum4-new']
 
-vars_dict = {x1(t) : X1, x2(t) : X2}
-    
-equations = [predicate.MetitEquation(x1,'t',deriv_dict,vars_dict),
-             predicate.MetitEquation(x2,'t',deriv_dict,vars_dict),
-             predicate.MetitEquation(-9.8*sin(x1),'t',deriv_dict,vars_dict),
-             #predicate.MetitEquation(x1-1,'t',deriv_dict,vars_dict),
-             predicate.MetitEquation(x2-15,'t',deriv_dict,vars_dict)]
-             #predicate.MetitEquation(0.3345*x2**2+1.4615*sin(x1)**2+1.7959*cos(x1)**2-6.689*cos(x1)+4.6931-15, 't',deriv_dict, vars_dict)]
+for file_name in filenames:
 
-e5 = predicate.MetitEquation(deriv_dict[x2.diff(t)],'t',deriv_dict,vars_dict)
-equations.extend(predicate.get_derivs(1,e5))
+    for metit_timeout in [10, 100, 1000,4000]:
+        cur_exp = experiment.Experiment(file_name,metit_timeout)        
+    #execfile('quantum6.py',globals())
 
-feasible = 0
-infeasible = 0
-oplist = ['>','=','<']
-inftest = []
-    
-for equation in equations:
-    predlist = [predicate.MetitPredicate(equation,op) for op in oplist]
-    inftest.append(predlist)
-
-system = [predicate.State('X1,X2',n,*element) for n,element in enumerate(product(*inftest))]
-
-now = datetime.datetime.now()
-directory_name = now.strftime('%d-%m-%Y--%H:%M:%S')
-
-os.makedirs('/opt/quantum/'+ directory_name + '/firstpass/proved')
-os.makedirs('/opt/quantum/'+ directory_name + '/firstpass/unproved')
-
-for state in system:
-    #print metitarski.make_fof_inf(state)
-    print "checking state %s"  % state.get_state_number()
-    fof = metitarski.make_fof_inf(state)
-    rc = metitarski.send_to_metit(fof,output=True,tofile=False)
-    if rc == 0:
-        infeasible = infeasible+1
-        state.is_feasible = False
-        print 'it is not feasible, proved'
-    else:
-        feasible = feasible+1
-        print 'it is feasible, unproved'
-        metitarski.send_to_file(fof, directory_name + '/firstpass/unproved', '%s' % state.number)
-
-print "Feasible %s" % feasible
-print "Infeasible %s" % infeasible
-
-'''print "Second Run"
-
-feasible = 0
-infeasible = 0
-
-options = ('metit', 
-           '--autoInclude', 
-           '--time','30',
-           '-')
-
-os.makedirs('/opt/quantum/'+ directory_name + '/secondpass/proved')
-os.makedirs('/opt/quantum/'+ directory_name + '/secondpass/unproved')
-
-for state in system:
-    #print metitarski.make_fof_inf(state)
-    if state.is_feasible:
-        print "checking state %s"  % state.get_state_number()
-        fof = metitarski.make_fof_inf(state)
-        rc = metitarski.send_to_metit(fof,output=False,tofile=False,metit_options=options)
-        if rc == 0:
-            infeasible = infeasible+1
-            state.is_feasible = False
-        else:
-            feasible = feasible+1
-            metitarski.send_to_file(fof, directory_name + '/secondpass/unproved', '%s' % state.number)
-
-print "Feasible %s" % feasible
-print "Infeasible %s" % infeasible
-'''
-system_f = [state for state in system if state.is_feasible]
-
-#print 'Press -ENTER- to continue'
-#raw_input()
-
-def find_states(state_list, preds):
-    for sta in preds:
-        return [x for x,state in enumerate(state_list) if all(i in sta for i in state.state)]
-    
-for state in system_f:
-    pos_successors = []
-    
-    for pred in state.state:
-        #print pred.operator
-        pos_successors.append(metitarski.checkTransition(state,pred))
-
-    #print pos_successors
-    #print "State %s :" % list(product(*pos_successors))
-
-    #for state in product(*pos_successors)
-
-    nstate = []
-    
-    for state2 in product(*pos_successors):
-        #print state
-        ss = predicate.State('X1,X2',666,*state2)
-        #print ss
+        print cur_exp.system_def
         
-        for s in system_f:
-            if s == ss:
-                nstate.append(s.number)
-            #else:
-            #    print 'no state found'
+        cur_exp.metit_options = ['metit', 
+                 '--autoInclude', 
+                 '--time',str(metit_timeout),
+                 '-q']
+        
+        experiment.create_exp_dirs(cur_exp)
 
-    print "From State %s Next State %s" % (state.number,nstate)
-    state.next_states = nstate
-   # print find_states(system_f,product(*pos_successors))
+        feas_check_proved_dir = cur_exp.feas_check_proved_dir
+        feas_check_unproved_dir = cur_exp.feas_check_unproved_dir
+        cont_trans_proved_dir =  cur_exp.cont_trans_proved_dir
+        cont_trans_unproved_dir = cur_exp.cont_trans_unproved_dir
+        disc_trans_proved_dir = cur_exp.disc_trans_proved_dir
+        disc_trans_unproved_dir = cur_exp.disc_trans_unproved_dir
+        
+        start_time = time.time()    
+        f = open('log.txt', 'a', 0)
+        f.write(40*'*'+'\n')
+        f.write(cur_exp.filename + '-' + str(metit_timeout) + '-' + '\n')
+        f.write(40*'*' + '\n')
 
-nusmv.construct_nusmv_input(system_f,23)
-timing.endlog()
+        print 'starting quantum6'
+        
+        hybrid_system = abstraction.initial_abstract_system_setup(cur_exp.equations, cur_exp.q, cur_exp.system_def)
+        var_string = predicate.get_var_string(cur_exp.equations)
+
+    #initial_state_numbers = abstraction.conc_to_abs(hybrid_system,('falling',),'VY=0','PY<0','G<0','-G - PY + sin(PX)=0','PX<0','VX=0')
+
+    #initial_state_numbers = abstraction.conc_to_abs(hybrid_system,('falling',),'-H + PY<0','VY=0','VX=0')
+
+        initial_state_numbers = abstraction.conc_to_abs(hybrid_system,cur_exp.initial_state['d'],cur_exp.initial_state['c'])
+
+        f.write('Number of Abstraction Functions : %s\n' % len(cur_exp.equations))
+        f.write('Number of Initial Abstract States : %s\n' % len(hybrid_system))
+    
+        next_states = [state_num for state_num in initial_state_numbers if abstraction.is_state_feasible(hybrid_system[state_num], var_string,feas_check_proved_dir, feas_check_unproved_dir,cur_exp)]
+        
+        ## LAZY QUAL ABS ##
+
+        #bad = predicate.MetitPredicate(py-h,'>')
+        #bad2 = predicate.MetitPredicate(0.5*vx**2+0.5*vy**2+2*9.8*py-2*9.8*sin(px)-9.8,'>')
+    
+        if not(abstraction.lazy_cont_abs(hybrid_system, next_states, cur_exp.system_def, var_string, cur_exp, bad_predicate=cur_exp.bad_state)):
+            f.write('**PROP VIOLATED**')
+        else:
+            SMV = open(cur_exp.filename + '-' + str(metit_timeout) + '.smv','w')
+            
+            smv_output = nusmv.construct_nusmv_input(hybrid_system,2)
+            
+            SMV.write(smv_output)
+            SMV.close()
+            
+            end_time = time.time()
+
+    
+            f.write('Number of Final Abstract States : %s\n' % len([x for x in hybrid_system.itervalues() if x.is_feasible and x.feasability_checked and x.next_states]))
+            f.write('Number of UnProved InFeasible : %s\n' % cur_exp.infeas_unproved)
+            f.write('Number of Proved Infeasible : %s\n' % cur_exp.infeas_proved)
+            f.write('Number of Proved Transitions : %s\n' % cur_exp.trans_proved)
+            f.write('Number of UnProved Transitions : %s\n' % cur_exp.trans_unproved)        
+            f.write('Total Time taken : %s\n' % qutilities.secondsToStr(end_time-start_time))
+            f.close()
+            
+            abstraction.print_system(hybrid_system)
+
+    
+
+
+

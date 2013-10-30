41d878cc2d2fcc8a576cd146e0964c6df883fea1
William Denman <william.denman@gmail.com>
2013-10-30 12:05:11 +0000

    adds top_level support for quantum

--- top_level.py
+++ top_level.py
@@ -1,5 +1,6 @@
 import code
 import cmd, sys
+import quantum
 
 #my_console = code.InteractiveConsole()
 #my_console.interact('Quantum Top Level')
@@ -9,11 +10,37 @@ class CLI(cmd.Cmd):
     
     def __init__(self):
         cmd.Cmd.__init__(self)
-        self.prompt = '(QUANTUM) >'
+        self.prompt = '(QUANTUM) > '
 
     def do_quit(self, arg):
         sys.exit(1)
+    
+    def do_abstract(self, arg):
+        try:
+            #print type(arg)
+            quantum.run(arg.split())
+        except KeyboardInterrupt:
+            print 'abstraction paused'
+            
+            
+    def do_print(self, arg):
+        #print quantum.filenames
+        print quantum.cur_exp.system_def
+    
+    def do_stats(self, arg):
 
+        hs = quantum.hybrid_system
+
+        print '*'*40
+        print 'Abstraction Proof Statistics'
+        print '*'*40
+
+        print 'Total Number of States : {}'.format(len([x for x in hs.itervalues() if x.is_feasible and x.feasability_checked and x.next_states]))
+        print 'Total Number of Proved Transition Problems : {}'.format(quantum.cur_exp.trans_proved)
+        print 'Total Number of Unproved Transition Problems : {}'.format(quantum.cur_exp.trans_unproved)
+        print 'Total Number of Proved Infeasible Problems : {}'.format(quantum.cur_exp.infeas_proved)
+        print 'Total Number of Unproved Infeasible Problems : {}'.format(quantum.cur_exp.infeas_unproved)
+                                                                    
     def do_run(self, arg):
         
         try:
@@ -21,6 +48,9 @@ class CLI(cmd.Cmd):
                 print 'hello world'
         except KeyboardInterrupt:
             print 'done'
+
+    # shortcuts
+    do_q = do_quit
 
 if __name__ == '__main__':
     cli = CLI()

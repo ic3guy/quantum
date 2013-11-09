d7d3d71620408fd5bfca586de168023f184d285a
William Denman <william.denman@gmail.com>
2013-11-09 01:43:52 +0000

    new file modifications

--- examples/twotanks-new2.py
+++ examples/twotanks-new2.py
@@ -31,14 +31,15 @@ e6 = x2-0.5
 e7 = (x1-4.25)**2+(x2-0.25)**2-0.0625
 
 equations = [predicate.MetitEquation(ge,var_id=2),
-             predicate.MetitEquation(x2),
-             predicate.MetitEquation(e3),
+             predicate.MetitEquation(x2,var_id=2),
+             predicate.MetitEquation(e3,var_id=2),
              predicate.MetitEquation(e2, var_id=1),
              predicate.MetitEquation(e7),
              predicate.MetitEquation(e1,var_id=1),
-             predicate.MetitEquation(e4),
-             predicate.MetitEquation(e5),
-             predicate.MetitEquation(e6)]
+             predicate.MetitEquation(e4,var_id=1),
+             predicate.MetitEquation(e5, var_id=1),
+             predicate.MetitEquation(e6,var_id=2),
+            ]
 
 
 initial_state = {'d':('s1',),'c': [str(predicate.MetitPredicate(*x)) for x in
@@ -47,7 +48,8 @@ initial_state = {'d':('s1',),'c': [str(predicate.MetitPredicate(*x)) for x in
                                            (e7,'>'),
                                            (e4,'>'),
                                            (e5,'<'),
-                                           (e6,'<')]]}
+                                           (e6,'<'),
+                                           (e1,'>')]]}
 
 
 #['X1 - 5.25>0','X1 - 5.75<0','X2 - 1<0','(X1 - 4.25)^2 + (X2 - 0.25)^2 - 0.0625>0']}
@@ -65,15 +67,15 @@ system_def = {('s1',) : {'flow' : { x1.diff(t): 1 - sqrt(x1),
                                {'guard':([MetitPredicate(e7,'=')],),
                                 'next_state': ('unsafe',),
                                 'updates' : {}}],
-                         'inv' : (MetitPredicate(x1-4,'<'),MetitPredicate(x1-6,'>'),MetitPredicate(x2,'<'),MetitPredicate(x2-1,'>'),MetitPredicate(x2-2,'>'), MetitPredicate(x2-2,'=')),
-                         'colour':'blue'},
+                         'inv' : (MetitPredicate(x1-4,'<'),MetitPredicate(x1-6,'>'),MetitPredicate(x2,'<'),MetitPredicate(x2-1,'>'),MetitPredicate(x2-2,'>'), MetitPredicate(x2-2,'='), MetitPredicate(e7,'<')),
+                         'colour':'lightblue'},
               ('s2',) : {'flow' : { x1.diff(t): 1 - sqrt(x1-x2+1),
                                     x2.diff(t): sqrt(x1-x2+1)-sqrt(x2)}
                          ,
                          't' : [{'guard':([guard],),
                                  'next_state' : ('s1',),
                                  'updates' : {}}],
-                         'inv' : (MetitPredicate(x1-4,'<'),MetitPredicate(x1-6,'>'),MetitPredicate(x2-1,'<'),MetitPredicate(x2-2,'>'),MetitPredicate(x2,'<'),MetitPredicate(x2,'='),MetitPredicate(x2-0.5,'<'),MetitPredicate(x2-0.5,'=')),
+                         'inv' : (MetitPredicate(x1-4,'<'),MetitPredicate(x1-6,'>'),MetitPredicate(x2-1,'<'),MetitPredicate(x2-2,'>'),MetitPredicate(x2,'<'),MetitPredicate(x2,'='),MetitPredicate(x2-0.5,'<'),MetitPredicate(x2-0.5,'='),MetitPredicate(e7,'<')),
                          'colour':'green'}}
               # ('unsafe',): {'flow' : {x1.diff(t) : 0,
               #                         x2.diff(t) : 0},
@@ -87,6 +89,7 @@ x1d = predicate.metit_derivative(x1eq, ('s1',), system_def)
 
 x2eq = predicate.MetitEquation(sqrt(x1)-sqrt(x2))
 x2d = predicate.metit_derivative(x2eq, ('s1',), system_def)
+
 
 
 if __name__ == '__main__':

d226a9cc7abf08d9057232b4bbea7a7f8f4a5501
William Denman <william.denman@gmail.com>
2013-11-15 16:32:20 +0000

    new example, try to get working

--- examples/satel3.py
+++ examples/satel3.py
@@ -13,40 +13,45 @@ nu2 = Function('nu2')(t)
 q = [('initial','rendezvous')] #can just get dictionary keys...
 
 bad = False
-extra_constraints = ['NU1<300','NU2<300']
+extra_constraints = ['NU1<6','NU2<6']
 #extra_constraints = ''
 bad_state = ''
 
 def nudot(nu, p, e):
     b = ssqrt(1.0/(p**3))
-    e = (1+e*cos(nu * 0.0175))**2
+    e = (1+e*cos(nu))**2
     
     return b*e
 
 def nu_x(nu, p, e):
-    r = p/(1+e*cos(nu*0.0175))
+    r = p/(1+e*cos(nu))
     
-    return r*cos(nu*0.0175)
+    return r*cos(nu)
 
 def nu_y(nu, p, e):
-    r = p/(1+e*cos(nu*0.0175))
+    r = p/(1+e*cos(nu))
     
-    return r*sin(nu*0.0175)
+    return r*sin(nu)
 
 def dist(nu1, p1, e1, nu2, p2, e2):
     return (nu_x(nu1,p1,e1)-nu_x(nu2,p2,e2))**2 + (nu_y(nu1,p1,e1)-nu_y(nu2,p2,e2))**2
 
-e1 = nu1 - 250
-e2 = nu2 - 50
-e4 = nu1 - 300
+e1 = nu1 - 3
+e2 = nu2 - 1
+e4 = nu1 - 4
 
-e3 = dist(nu1,7074,0.05,nu2,7748,0.10) - 2500000
+e3 = dist(nu1,7467,0.10,nu2,7670,0.10) - 2500000
+#e3 = 4.124 + 0.817*nu1 - nu2
+
+#this better1
+#e3 = -137.513 + 1.59823*nu1 - 0.00142977*nu1**2 - nu2
+
 
 e6 = 1.1*nu1 - nu2 - 50
 e7 = nu1 - 90
 
-nd1 = nudot(nu1, 7074,0.05)
-nd2 = nudot(nu2, 7748,0.10)
+nd1 = nudot(nu1, 7467,0.10)
+nd2 = nudot(nu2, 7670,0.10)
 
 
 equations = [predicate.MetitEquation(nu1),
@@ -68,10 +73,10 @@ initial_state = {'d':('initial',),'c': [str(predicate.MetitPredicate(*x)) for x 
 
 system_def = {('initial',) : {'flow' : { nu1.diff(t): nd1,
                                          nu2.diff(t): nd2},
-                              't' : [{'guard':([MetitPredicate(dist(nu1,7074,0.05,nu2,7748,0.10) - 2500000,'=')],), 
+                              't' : [{'guard':([MetitPredicate(e3,'=')],), 
                                       'next_state' : ('rendezvous',),
                                       'updates' : {}}],
-                              'inv' : [MetitPredicate(dist(nu1,7074,0.05,nu2,7748,0.10) - 2500000,'<')],
+                              'inv' : [MetitPredicate(e3,'<')],
                               'colour':'green'},
               ('rendezvous',) : {'flow' : { nu1.diff(t): 0,
                                             nu2.diff(t): 0},

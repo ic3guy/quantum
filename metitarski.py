6a6acf7507eb38a97c6338478bf247e070aa9535
William Denman <william.denman@gmail.com>
2014-03-06 16:02:45 +0000

    fixes regexp

--- metitarski.py
+++ metitarski.py
@@ -147,7 +147,7 @@ def pred_2_text(pred):
     elif pred == '=':
         return 'eq'
 
-def cont_abs_trans_rel(state, pred, exp, subsdict={'exp':'10^'}):
+def cont_abs_trans_rel(state, pred, exp, subsdict={'e[^x]':'*10^'}):
 
     #use multiprocessing on calling this function.
 

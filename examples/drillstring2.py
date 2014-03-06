7c5a35d6e5d54e02e02ff625bd275e1f75a48922
William Denman <william.denman@gmail.com>
2014-03-06 11:39:25 +0000

    fixes formatting for drillstring2

--- examples/drillstring2.py
+++ examples/drillstring2.py
@@ -11,7 +11,7 @@ x3 = Function('x3')(t)
 
 #ct = Symbol('ct')
 
-q = [('slip+','slip-','stuck')] #can just get dictionary keys...
+q = [('slip+', 'slip-', 'stuck')]  # can just get dictionary keys...
 
 bad = False
 extra_constraints = ''
@@ -22,52 +22,55 @@ extra_constraints = ''
 
 Wob = 50000
 
-ueqTsb1 = 172.3067*x1 + 861.5336*x2 - (50 + 172.3067)*x3 - 0.12446*Wob
-ueqTsb2 = 172.3067*x1 + 861.5336*x2 - (50 + 172.3067)*x3 + 0.12446*Wob
-Absueq = 172.3067*x1 + 861.5336*x2 - (50 + 172.3067)*x3 - 0.122446*Wob
+ueqTsb1 = 172.3067 * x1 + 861.5336 * x2 - (50 + 172.3067) * x3 - 0.12446 * Wob
+ueqTsb2 = 172.3067 * x1 + 861.5336 * x2 - (50 + 172.3067) * x3 + 0.12446 * Wob
+Absueq = 172.3067 * x1 + 861.5336 * x2 - (50 + 172.3067) * x3 - 0.122446 * Wob
 
-slipp_to_slipn_guard = predicate.MetitPredicate(ueqTsb2,'<')
-slipn_to_slipp_guard = predicate.MetitPredicate(ueqTsb1,'>')
-absueq_guard = predicate.MetitPredicate(Absueq,'<')
+slipp_to_slipn_guard = predicate.MetitPredicate(ueqTsb2, '<')
+slipn_to_slipp_guard = predicate.MetitPredicate(ueqTsb1, '>')
+absueq_guard = predicate.MetitPredicate(Absueq, '<')
 
-x3_gtz = predicate.MetitPredicate(x3,'>')
-x3_ltz = predicate.MetitPredicate(x3,'<')
-x3_eqz = predicate.MetitPredicate(x3,'=')
+x3_gtz = predicate.MetitPredicate(x3, '>')
+x3_ltz = predicate.MetitPredicate(x3, '<')
+x3_eqz = predicate.MetitPredicate(x3, '=')
 
 
-initial_state = {'d' : ('slip+',),'c' : ['X1>0','X3>0']}
+initial_state = {'d': ('slip+',), 'c': ['X1>0', 'X3>0']}
 bad_state = []
 
-system_def = {('slip+',) : {'flow' : {x1.diff(t): (6000 - 597.3067*x1 - 861.5336*x2 + 172.3067*x3)/2122,
-                                      x2.diff(t): x1-x3,
-                                      x3.diff(t): ((-(0.155575*(0.5 + (-1*0.5 + 0.8)/exp(0.9*x3))*Wob) + 172.3067*x1 + 861.5336*x2 - (50 + 172.3067)*x3)/471.9698)},
-                            't'    : [{'guard'      : ([slipp_to_slipn_guard,x3_eqz],[x3_ltz]), 
-                                       'next_state' : ('slip-',),
-                                       'updates'    : {}},
-                                      {'guard'      : ([x3_eqz,absueq_guard],),
-                                       'next_state' : ('stuck',),
-                                       'updates' : {}}],
-                            'inv'  : (x3_ltz,)},
-              ('slip-',) : {'flow' : {x1.diff(t): (6000 - 597.3067*x1 - 861.5336*x2 + 172.3067*x3)/2122,
-                                      x2.diff(t): x1-x3,
-                                      x3.diff(t): ((0.155575*(0.5 + (-1*0.5 + 0.8)/exp(-0.9*x3))*Wob + 172.3067*x1 + 861.5336*x2 - (50 + 172.3067)*x3)/471.9698)},
-                            't'    : [{'guard'      : ([slipn_to_slipp_guard,x3_eqz],[x3_gtz]), 
-                                       'next_state' : ('slip+',),
-                                       'updates'    : {}},
-                                      {'guard'       : ([x3_eqz,absueq_guard],),
-                                       'next_state' : ('stuck',),
-                                       'updates' : {}}],
-                            'inv'  : (x3_gtz,)},
-              ('stuck',) : {'flow' : {x1.diff(t): (6000 - 597.3067*x1 - 861.5336*x2 + 172.3067*x3)/2122,
-                                      x2.diff(t): x1-x3,
-                                      x3.diff(t): 0},
-                            't'    : [{'guard'      : ([slipp_to_slipn_guard,x3_eqz],[x3_ltz]), 
-                                       'next_state' : ('slip-',),
-                                       'updates'    : {}},
-                                      {'guard'      : ([slipn_to_slipp_guard,x3_eqz],[x3_gtz]), 
-                                       'next_state' : ('slip+',),
-                                       'updates'    : {}}],
-                            'inv'  : (x3_ltz, x3_gtz)}}
+system_def = {('slip+',):
+              {'flow': {x1.diff(t): (6000 - 597.3067 * x1 - 861.5336 * x2 + 172.3067 * x3) / 2122,
+                        x2.diff(t): x1 - x3,
+                        x3.diff(t): ((-(0.155575 * (0.5 + (-1 * 0.5 + 0.8) / exp(0.9 * x3)) * Wob) + 172.3067 * x1 + 861.5336 * x2 - (50 + 172.3067) * x3) / 471.9698)},
+               't': [{'guard': ([slipp_to_slipn_guard, x3_eqz], [x3_ltz]),
+                      'next_state': ('slip-',),
+                      'updates': {}},
+                     {'guard': ([x3_eqz, absueq_guard],),
+                      'next_state': ('stuck',),
+                      'updates': {}}],
+               'inv': (x3_ltz,)},
+              ('slip-',):
+              {'flow': {x1.diff(t): (6000 - 597.3067 * x1 - 861.5336 * x2 + 172.3067 * x3) / 2122,
+                        x2.diff(t): x1 - x3,
+                        x3.diff(t): ((0.155575 * (0.5 + (-1 * 0.5 + 0.8) / exp(-0.9 * x3)) * Wob + 172.3067 * x1 + 861.5336 * x2 - (50 + 172.3067) * x3) / 471.9698)},
+               't': [{'guard': ([slipn_to_slipp_guard, x3_eqz], [x3_gtz]),
+                      'next_state': ('slip+',),
+                      'updates': {}},
+                     {'guard': ([x3_eqz, absueq_guard],),
+                      'next_state': ('stuck',),
+                      'updates': {}}],
+               'inv': (x3_gtz,)},
+              ('stuck',):
+              {'flow': {x1.diff(t): (6000 - 597.3067 * x1 - 861.5336 * x2 + 172.3067 * x3) / 2122,
+                        x2.diff(t): x1 - x3,
+                        x3.diff(t): 0},
+               't': [{'guard': ([slipp_to_slipn_guard, x3_eqz], [x3_ltz]),
+                      'next_state': ('slip-',),
+                      'updates': {}},
+                     {'guard': ([slipn_to_slipp_guard, x3_eqz], [x3_gtz]),
+                      'next_state': ('slip+',),
+                      'updates': {}}],
+               'inv': (x3_ltz, x3_gtz)}}
 
 equations = [predicate.MetitEquation(x1),
              predicate.MetitEquation(x3),
